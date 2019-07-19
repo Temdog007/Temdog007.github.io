888c23e031d9f7c301eb69944a0d049b5b83ba4f
Temdog007 <temdog007@yahoo.com>
2019-07-18 22:11:31 -0700

    Update GUI

--- g-arch/g-arch.js
+++ g-arch/g-arch.js
@@ -7,6 +7,24 @@ var architecture = JSON.parse(localStorage.getItem(storageKey)) || {
     families : []
 };
 
+class Component
+{
+    constructor(name)
+    {
+        if(typeof name != "string" || !name)
+        {
+            throw new Error("Must enter a name");
+        }
+        this.name = name;
+        this.properties = [
+            {
+                name : 'x',
+                type : 'integer'
+            }
+        ];
+    }
+}
+
 updateComponentsModel();
 updateJson();
 
@@ -17,13 +35,10 @@ $("#systems-button").click(() =>
 
 $("#components-button").click(() =>
 {
-    var component = {
-        name : `New Component${architecture.components.length}`
-    };
+    const component = new Component(`New Component${architecture.components.length}`);
     architecture.components.push(component);
-    save();
+
     updateComponentsModel();
-    updateJson();
 });
 
 $("#entities-button").click(() =>
@@ -36,6 +51,33 @@ $("#families-button").click(() =>
 
 });
 
+function updateComponentInput(evt)
+{
+    var t = evt.target;
+    var comp = architecture.components.find(c => c.name == $(t).attr('component-name'));
+    if(!comp)
+    {
+        return;
+    }
+
+    switch($(t).attr('target'))
+    {
+        case 'name':
+            comp.name = $(t).val();
+            break;
+        case 'property-name':
+            comp.properties[$(t).attr('index')].name = $(t).val();
+            break;
+        case 'property-type':
+            comp.properties[$(t).attr('index')].type = $(t).val();
+            break;
+        default:
+            return;
+    }
+    
+    updateComponentsModel();
+}
+
 function updateJson()
 {
     $("#jsonContent").html(JSON.stringify(architecture, undefined, 2));
@@ -43,47 +85,53 @@ function updateJson()
 
 function updateComponentsModel()
 {
-    var div = document.getElementById("components-data");
+    const div = document.getElementById("components-data");
     while(div.hasChildNodes())
     {
         div.lastChild.remove();
     }
 
-    for(var key in architecture.components)
+    for(var comp of architecture.components)
     {
         var compDiv = document.createElement("div");
-        var h = document.createElement("h5");
+
+        var h = document.createElement('h5');
+        h.innerHTML = `<input type='text' value='${comp.name}' component-name='${comp.name}' target='name' onclick='${updateComponentInput}'>`;
+        compDiv.appendChild(h);
 
         var table = document.createElement("table");
         compDiv.appendChild(table);
 
         table.innerHTML +=`
-            <table>
-            <tr>
-                <th>Key</th>
-                <th>Type</th>
-                <th>Value</th>
-            </tr>
+            <table class='table table-strpied'>
+                <tr>
+                    <th>Key</th>
+                    <th>Type</th>
+                </tr>
         `;
 
-        var comp = architecture.components[key];
-        for(var k in comp)
+        const properties = comp.properties;
+        for(var i = 0; i < properties.length; ++i)
         {
-            var val = comp[k];
+            var prop = properties[i];
             table.innerHTML += `
             <tr>
-                <td>${k}</td>
-                <td>${typeof val}</td>
-                <td>${val}</td>
+                <td><input type='text' value='${prop.name}' component-name='${comp.name}' target='property-name' index='${i}'></td>
+                <td>${prop.type}</td>
             </tr>
             `;
         }
         
-        div.appendChild(table);
+        div.appendChild(compDiv);
+
+        $('input', $(table)).each((_, t) =>  $(t).change(updateComponentInput));
     }
+    
+    save();
 }
 
 function save()
 {
     localStorage.setItem(storageKey, JSON.stringify(architecture));
+    updateJson();
 }
